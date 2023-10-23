5453156aa115589d278cd491e44772bcbf70ff95
Weronika Lalewicz (Uczeń) <wlalewicz562@technikumkreatywne.pl>
2023-10-23 13:11:07 +0200

    fajne zadanie

---
+++ Pierwsza lekcja/uwu 1.cpp
@@ -0,0 +1,24 @@
+ #include <iostream> 
+ using namespace std;
+ int main(){
+ 	short zmienna = 2 ;
+	cout <<"Hello world :P " <<zmienna ;
+
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+
+
+
+
+    return 0;
+}
