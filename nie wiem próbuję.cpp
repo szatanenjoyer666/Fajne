79ccaafb071d6f9ea4ef07a600bd8c2d44ad0770
Weronika Lalewicz (Uczeń) <wlalewicz562@technikumkreatywne.pl>
2024-01-08 13:14:04 +0100

    Spróbuj dokończyć bo wydaje się proste

---
+++ nie wiem próbuję.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	float liczba;
+	cout << "Podaj liczbe: ";
+	cin >> liczba;
+	cout endl;
+	
+	float liczbe;
+	cout << "Podaj drugą: ";
+	
+	
+	
+//najpierw pobierasz wszystkie dane i dopiero potem robi się te case'y
