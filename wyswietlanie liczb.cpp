228d5f345f72a7cfa385d252e1b85985a2b314e9
Vera <148436811+szatanenjoyer666@users.noreply.github.com>
2024-05-19 12:55:32 +0200

    wyswietlanie

---
+++ wyswietlanie liczb.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+
+using namespace std;
+
+int main( )
+{
+  int n, lp, p, d;
+  bool t;
+
+  cout << "Ile liczb pierwszych chcesz wyswietlic: ";
+  cin >> n;
+  lp = 0;
+  p  = 2;
+  cout << "Liczby pierwsze: ";
+  while( lp < n )
+  {
+    t = true;
+    for( d = 2; d < p; d++ ){
+      if( p % d == 0 )
+      {
+        t = false;
+ 
+      }
+    }
+    if( t )
+    {
+        cout << p << ", ";
+        lp++;
+    }
+    p++;
+  }
+  cout << endl;
+  return 0;
+} 
