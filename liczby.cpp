789bb30ba17a7a16492cb4fa2275b8103d98a6f6
Vera <148436811+szatanenjoyer666@users.noreply.github.com>
2024-05-23 23:32:06 +0200

    liczby.cpp

--- liczby.cpp
+++ liczby.cpp
@@ -2,13 +2,13 @@
 
 using namespace std;
 
-void rozklad(int liczba) {
+void rozklad(int l) {
     int k = 2; 
 
-    while (liczba > 1) {
-        while (liczba % k == 0) { //rozklad
+    while (l > 1) {
+        while (l % k == 0) { //rozklad
             cout << k << " ";
-            liczba = liczba / k;  //warunek spelniony
+            l = l / k;  //warunek spelniony
         }
         k = k + 1; //jak nie moze sie wykonac, jesli liczba % to nie 0
     }
@@ -21,6 +21,6 @@ int main() {
     cout << "Podaj liczbe: ";
     cin >> liczba;
     cout << "Czynniki pierwsze liczby " << liczba <<": ";
-    rozklad(liczba);
+    rozklad(liczba); //wywolywanie f, liczba przypis do zm L
     return 0;
 }
